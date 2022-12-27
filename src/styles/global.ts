5632e55a0e9b8d09a5e1dbd68665f385d81e7c04
Stephani Engel <stephaniengel18@gmail.com>
2022-12-27 02:05:00 -0400

    Creating the routes, the flow between the pages and starting to structure the application

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle, DefaultTheme } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${(props) =>
+          props.theme['green-500' as keyof DefaultTheme]};
+    }
+
+    body {
+        background: ${(props) => props.theme['gray-900' as keyof DefaultTheme]};
+        color: ${(props) => props.theme['gray-300' as keyof DefaultTheme]};
+    }
+
+    body, input, textarea, button {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    }
+`
